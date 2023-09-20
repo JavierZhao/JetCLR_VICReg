f479a3dc5232efc88ce699780a3cfdad836a4745
JavierZhao <zhaoziha@usc.edu>
2023-09-20 09:50:39 -0700

    added validation in LCT

--- src/features/perf_eval.py
+++ src/features/perf_eval.py
@@ -48,9 +48,19 @@ def linear_classifier_test(
     trlab_in,
     reps_te_in,
     telab_in,
+    val_fraction=0.1,      # Fraction of training data to use as validation
     linear_opt="adam",
 ):
     xdevice = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    # Splitting training data into training and validation sets
+    num_val_samples = int(val_fraction * reps_tr_in.shape[0])
+    shuffled_indices = torch.randperm(reps_tr_in.shape[0])
+    reps_val_in = reps_tr_in[shuffled_indices[:num_val_samples]]
+    vallab_in = trlab_in[shuffled_indices[:num_val_samples]]
+    reps_tr_in = reps_tr_in[shuffled_indices[num_val_samples:]]
+    trlab_in = trlab_in[shuffled_indices[num_val_samples:]]
+
     fcn_linear = fully_connected_linear_network(
         linear_input_size, 1, linear_opt, linear_learning_rate
     )
@@ -58,15 +68,19 @@ def linear_classifier_test(
     bce_loss = nn.BCELoss()
     sigmoid = nn.Sigmoid()
     losses = []
+    val_losses = []    # List to store validation losses
+    
     if linear_opt == "sgd":
         scheduler = torch.optim.lr_scheduler.StepLR(
             fcn_linear.optimizer, 100, gamma=0.6, last_epoch=-1, verbose=False
         )
+    
     for epoch in range(linear_n_epochs):
         indices_list = torch.split(
             torch.randperm(reps_tr_in.shape[0]), linear_batch_size
         )
         losses_e = []
+        
         for i, indices in enumerate(indices_list):
             fcn_linear.optimizer.zero_grad()
             x = reps_tr_in[indices, :]
@@ -78,9 +92,19 @@ def linear_classifier_test(
             loss.backward()
             fcn_linear.optimizer.step()
             losses_e.append(loss.detach().cpu().numpy())
+        
+        # Calculate validation loss at the end of the epoch
+        with torch.no_grad():
+            x_val = torch.Tensor(reps_val_in).view(-1, linear_input_size).to(xdevice)
+            l_val = torch.Tensor(vallab_in).float().view(-1, 1).to(xdevice)
+            z_val = sigmoid(fcn_linear(x_val)).to(xdevice)
+            val_loss = bce_loss(z_val, l_val).to(xdevice)
+            val_losses.append(val_loss.detach().cpu().numpy())
+
         losses.append(np.mean(np.array(losses_e)))
         if linear_opt == "sgd":
             scheduler.step()
+            
     out_dat = (
         fcn_linear(torch.Tensor(reps_te_in).view(-1, linear_input_size).to(xdevice))
         .detach()
@@ -88,4 +112,4 @@ def linear_classifier_test(
         .numpy()
     )
     out_lbs = telab_in
-    return out_dat, out_lbs, losses
+    return out_dat, out_lbs, losses, val_losses    # Return the validation losses as well
